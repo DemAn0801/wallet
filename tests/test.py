8260acf8ef2483a4fb3a8e9bb456104817ae0b2b
Dementyev_Andrey <dem.an.a@yandex.ru>
2024-05-07 13:11:18 +0500

    test read file, create row

--- tests/test.py
+++ tests/test.py
@@ -7,19 +7,27 @@ class TestFileManager(unittest.TestCase):
     file_test_append = "tests/test_files/for_append/append_test.csv"
     def test_read(self):
         fm = FileManager()
-        result: list = [x for x in fm.read_file(self.file_balance)]
+        result: list = [x for x in fm.read_file(self.file_balance_read)]
         self.assertEqual(result[0]["Баланс"], "7000")
         
     def test_append_row(self):
-      fm = FileManager()
-      data = [
-              {
-                  "Дата": "2022-12-01",
-                  "Категория": "Расход",
-                  "Описание": "Квартплата",
-                  "Сумма": "1000",
-              }
-      ],
-      fm.create_row(file_name=self.file_test_append, row=data, ) 
+        fm = FileManager()
+        data = [
+                {
+                    "Дата": "2022-12-01",
+                    "Категория": "Расход",
+                    "Описание": "Квартплата",
+                    "Сумма": "1000",
+                }
+        ]
+        fm.create_row(file_name=self.file_test_append, row=data)
+        result=""
+        with open(self.file_test_append, "r") as f:
+            result = f.read()
+        with open(self.file_test_append, "w") as f:
+            f.write("")
+        self.assertEqual(result, "2022-12-01;Расход;Квартплата;1000\n") 
+      
+      
 if __name__ == "__main__":
   unittest.main()
